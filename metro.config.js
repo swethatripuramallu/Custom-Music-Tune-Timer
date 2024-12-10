b5a448ce04794931d2268be88af7b658c6a5ffe6
Margaret Solinsky <msolinsk@purdue.edu>
2024-12-09 23:23:28 -0500

    small bug

--- metro.config.js
+++ metro.config.js
@@ -1,34 +1,20 @@
-// // const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+// /**
+//  * Metro configuration
+//  * https://metrobundler.dev/docs/configuration
+//  *
+//  * @type {import('metro-config').MetroConfig}
+//  */
+// const config = {};
 
-// // /**
-// //  * Metro configuration
-// //  * https://metrobundler.dev/docs/configuration
-// //  *
-// //  * @type {import('metro-config').MetroConfig}
-// //  */
-// // const config = {};
+// module.exports = mergeConfig(getDefaultConfig(__dirname), config);
 
-// // module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+// metro.config.js
+const { getDefaultConfig } = require('@expo/metro-config');
 
-// // metro.config.js
-// const { getDefaultConfig } = require('@expo/metro-config');
+module.exports = (async () => {
+  const config = await getDefaultConfig(__dirname);
 
-// module.exports = (async () => {
-//   const config = await getDefaultConfig(__dirname);
+  config.resolver.experimentalImportSupport = true; // Enable the experimental require.context feature
 
-//   config.resolver.experimentalImportSupport = true; // Enable the experimental require.context feature
-
-//   return config;
-// })();
-
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
-
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
-const config = {};
-
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+  return config;
+})();
